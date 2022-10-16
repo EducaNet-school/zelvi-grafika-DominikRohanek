b2061abfc9ceed48d4eab0e1dc92d18305b9a5b3
12-pp <113425694+12-pp@users.noreply.github.com>
2022-10-16 18:24:41 +0200

    poznamky

--- SeznamySimple1.py
+++ SeznamySimple1.py
@@ -8,4 +8,6 @@ for i in range(x):
 
     index = index+2
 
-    print(index+2)
+    print(index+2) #proc je tam plus 2
+#zbytecne prazdne radky, tiskne licha cisla, ne prvocisla
+#neukladas do seznamu
